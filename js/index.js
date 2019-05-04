1cb5f0cdf244b64b7a0d7f0bac0ec6826de6cbed
Zac Smith <zrs3141592@gmail.com>
2019-05-04 13:50:05 -0600

    finish 7 using setTimeout

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ busImg.addEventListener('mouseleave', (event) => {
     event.target.style.opacity = 1;
 });
 
-// 5 and 6
+// 5 and 6 hide footer with mouseover, the unhide with image mouseover
 const footerGone = document.querySelector('.footer p');
 const imgFooterFix = document.querySelector('.content-destination img');
 
@@ -45,4 +45,32 @@ imgFooterFix.addEventListener('mouseover', () => {
     footer.style.display = '';
 });
 
-//
+// 7 hide content-section based on click, use setTimeout
+
+const imgsGone = document.querySelector('.intro h2');
+
+imgsGone.addEventListener('click', (event) => {
+    // const imgs = document.querySelectorAll('.img-content img');
+    const content = document.querySelectorAll('.content-section');
+    // imgs[0].style.display = 'none';
+    // imgs.forEach(function(item) {
+    //     item.style.display = 'none';
+    // });
+
+    // setTimeout(function() {
+    //     imgs.forEach(function(item) {
+    //         item.style.display = '';
+    //     });
+    // }, 5000);
+
+    content.forEach(function(item) {
+        item.style.display = 'none';
+    });
+
+    setTimeout(function() {
+        content.forEach(function(item) {
+           item.style.display = '';
+        });
+    }, 5000);
+
+}, false);
