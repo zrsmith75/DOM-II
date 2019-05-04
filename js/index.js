d7497cf5cb59a8f96b2c2a023c6c11d165f3ed7f
Zac Smith <zrs3141592@gmail.com>
2019-05-03 19:23:46 -0600

    complete first event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+// let navWords = document.querySelectorAll('a');
+// navWords.forEach(words => words.style.color = "red");
+
+let navBar = document.querySelector('.nav');
+
+navBar.addEventListener('mouseover', function(event) {
+    event.target.style.color = "red";
+
+    setTimeout(function() {
+       event.target.style.color = "";
+    },2000);
+}, false);
+
+let funBusLogo = document.querySelector('.logo-heading');
+
+funBusLogo.addEventListener('click', function() {
+    
+});
